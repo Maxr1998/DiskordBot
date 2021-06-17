2815005bf0f727b7edc9ebcfedd1c14272f9f1f7
Maxr1998 <max.rumpf1998@gmail.com>
2021-06-18 01:13:07 +0200

    Fix custom emoji reaction

--- src/main/kotlin/de/maxr1998/diskord/Ext.kt
+++ src/main/kotlin/de/maxr1998/diskord/Ext.kt
@@ -1,7 +1,7 @@
 package de.maxr1998.diskord
 
 import com.jessecorbett.diskord.api.common.Emoji
-import com.jessecorbett.diskord.api.common.formatted
+import com.jessecorbett.diskord.api.common.stringified
 
 fun Config.getAckEmoji(): String =
-    ackEmojiId?.let { id -> Emoji(id = id).formatted } ?: Constants.DEFAULT_ACK_EMOJI
+    ackEmojiId?.let { id -> Emoji(id = id).stringified } ?: Constants.DEFAULT_ACK_EMOJI
