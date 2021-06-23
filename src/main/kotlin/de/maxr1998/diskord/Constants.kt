cf4b7cde5ea1bc60a6ab3d4b00d1d2a2bd7e57a0
Maxr1998 <max.rumpf1998@gmail.com>
2021-06-23 14:22:54 +0200

    Group commands in constants

--- src/main/kotlin/de/maxr1998/diskord/Constants.kt
+++ src/main/kotlin/de/maxr1998/diskord/Constants.kt
@@ -7,18 +7,24 @@ object Constants {
 }
 
 object Command {
+    // User management
     const val PROMOTE_ADMIN = "promoteadmin"
     const val PROMOTE_ADMIN_SHORT = "pa"
     const val PROMOTE = "promote"
     const val PROMOTE_SHORT = "p"
+
+    // AR management
     const val AUTO_RESPONDER = "autoresponder"
     const val AUTO_RESPONDER_SHORT = "ar"
     val AUTO_RESPONDER_MODE_ADD = arrayOf("add", "create", "new")
     val AUTO_RESPONDER_MODE_LIST = arrayOf("list", "ls")
     val AUTO_RESPONDER_MODE_REMOVE = arrayOf("remove", "rm", "delete")
 
+    // AR content management
     const val ADD = "add"
     const val REMOVE = "remove"
     const val REMOVE_SHORT = "rm"
+
+    // Help
     const val HELP = "help"
 }
