ad17cda5a83e6bbfe54135e4056bf8bdd710521c
Maxr1998 <max.rumpf1998@gmail.com>
2021-07-01 21:56:43 +0200

    Extract command constants to separate file

---
+++ src/main/kotlin/de/maxr1998/diskord/Command.kt
@@ -0,0 +1,27 @@
+package de.maxr1998.diskord
+
+object Command {
+    // User management
+    const val PROMOTE_ADMIN = "promoteadmin"
+    const val PROMOTE_ADMIN_SHORT = "pa"
+    const val PROMOTE = "promote"
+    const val PROMOTE_SHORT = "p"
+
+    // AR management
+    const val AUTO_RESPONDER = "autoresponder"
+    const val AUTO_RESPONDER_SHORT = "ar"
+    val AUTO_RESPONDER_MODE_ADD = arrayOf("add", "create", "new")
+    val AUTO_RESPONDER_MODE_LIST = arrayOf("list", "ls")
+    val AUTO_RESPONDER_MODE_REMOVE = arrayOf("remove", "rm", "delete")
+
+    // AR content management
+    const val ADD = "add"
+    const val REMOVE = "remove"
+    const val REMOVE_SHORT = "rm"
+
+    // Helper commands
+    const val RESOLVE = "resolve"
+
+    // Help
+    const val HELP = "help"
+}
--- src/main/kotlin/de/maxr1998/diskord/Constants.kt
+++ src/main/kotlin/de/maxr1998/diskord/Constants.kt
@@ -5,29 +5,3 @@ object Constants {
     const val COMMAND_PREFIX = "%"
     const val DEFAULT_ACK_EMOJI = "\uD83D\uDC4D"
 }
-
-object Command {
-    // User management
-    const val PROMOTE_ADMIN = "promoteadmin"
-    const val PROMOTE_ADMIN_SHORT = "pa"
-    const val PROMOTE = "promote"
-    const val PROMOTE_SHORT = "p"
-
-    // AR management
-    const val AUTO_RESPONDER = "autoresponder"
-    const val AUTO_RESPONDER_SHORT = "ar"
-    val AUTO_RESPONDER_MODE_ADD = arrayOf("add", "create", "new")
-    val AUTO_RESPONDER_MODE_LIST = arrayOf("list", "ls")
-    val AUTO_RESPONDER_MODE_REMOVE = arrayOf("remove", "rm", "delete")
-
-    // AR content management
-    const val ADD = "add"
-    const val REMOVE = "remove"
-    const val REMOVE_SHORT = "rm"
-
-    // Helper commands
-    const val RESOLVE = "resolve"
-
-    // Help
-    const val HELP = "help"
-}
