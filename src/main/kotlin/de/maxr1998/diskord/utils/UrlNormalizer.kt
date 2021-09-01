be967428efc0df3f59c3d7a8590174a4cbbf1516
Maxr1998 <max.rumpf1998@gmail.com>
2021-09-01 20:48:29 +0200

    Normalize Discord image URLs

--- src/main/kotlin/de/maxr1998/diskord/utils/UrlNormalizer.kt
+++ src/main/kotlin/de/maxr1998/diskord/utils/UrlNormalizer.kt
@@ -7,6 +7,7 @@ object UrlNormalizer {
     private val replacements = listOf(
         Regex("""($TWITTER_IMAGE_BASE_URL[\w_-]+)(?:\?format=|\.)([a-z]+)(?:[&?]name=|:)(?:[a-z]+|\d+x\d+)""") to "$1?format=$2&name=orig",
         Regex("""($PINTEREST_IMAGE_BASE_URL)/\d+x/([a-f\d]{2}/[a-f\d]{2}/[a-f\d]{2}/[a-f\d]{32}\.[a-z]+)""") to "$1/originals/$2",
+        Regex("""https://media.discordapp.net/(attachments/\d+/\d+/\S+\.\w+)""") to "https://cdn.discordapp.com/$1",
     )
 
     fun normalizeUrls(content: String): String = replacements.fold(content) { acc, (search, replace) ->
